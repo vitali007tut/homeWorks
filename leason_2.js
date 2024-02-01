b0b56fab7af48de038e11eba82672bfa5d3f6f7d
vitali007tut <vitali007@tut.by>
2024-02-01 21:58:20 +0300

    feat: add structuredClone description

--- leason_2.js
+++ leason_2.js
@@ -36,7 +36,46 @@ const counterCopy = _.cloneDeep(counter)
 
 Задание 3 – Создать функцию makeCounter всеми описанными и возможными способами;
 
+FD
+function makeCounter() {
+    console.log('counter is ready')
+    return 'counter'
+}
+
+FE
+const makeCounter = function() {
+    console.log('counter is ready')
+    return 'counter'
+}
+
+NFE
+const makeCounter = function innerFunc(who) {
+    if (who) {
+        console.log(`Counter ${who}`)
+    } else {
+        console.log('counter is ready without who')
+    }
+}
+
+Arrow function
+const makeCounter = () => console.log('counter is ready')
+
 Задание 4 - прочитать и описать работу глобальной функции structuredClone()
+
+    structuredClone() может:
+        - Клонировать бесконечно вложенные объекты и массивы.
+        - Клонировать циклические ссылки.
+        - Клонировать широкий спектр типов JavaScript, таких как: Date, Set, Map, Error, RegExp, ArrayBuffer, Blob, File, ImageData и многие другие.
+        - Передавать любые передаваемые объекты.
+
+    structuredClone не может клонировать:
+        - Функции
+        - Узлы DOM
+        - Дескрипторы свойств, сеттеры и геттеры
+        - Прототипы объектов (экземпляры классов)
+
+     structuredClone(value) -- value - Объект, который нужно клонировать
+     structuredClone(value, options) -- options - Объект со свойствам transfer - массив объектов которые будут перемещены
 
 Бонус
 Задание 1 –
@@ -61,12 +100,4 @@ function reverseStr(str) {
   return …
 } */
 
-const counter = { 
-    value: 0,
-    name: "Vitali",
-    sayHello: function() { console.log(`Hello, ${this.name}!`)}
-}
-const counterCopy = structuredClone(counter)
-console.log(counter, counterCopy)
-counter.sayHello()
-counterCopy.sayHello()
+const  reverseStr = (str) => str.split('').reverse().join('')
