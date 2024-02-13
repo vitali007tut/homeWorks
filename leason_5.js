5d8adacbc481009997838f24ced899ebccdd5a56
vitali007tut <vitali007@tut.by>
2024-02-13 21:56:28 +0300

    feat: add function fetchUrl

--- leason_5.js
+++ leason_5.js
@@ -161,3 +161,39 @@ timerFromArrayInterval([10, 12, 15, 21])
 //     .then(...)
 //     .catch(...) // сatch должен сработать только после 5 неудачных попыток
 // получить содержимое страницы внутри fetchUrl
+function fetchUrl(url) {
+    return new Promise((resolve, reject) => {
+        let start = 0;
+        let finish = 5;
+
+        async function letsGo() {
+            try {
+                const response = await fetch(url);
+                if (response.ok) {
+                    const result = await response.json();
+                    console.log(result);
+                    resolve(result);
+                }
+
+            } catch (error) {
+                start++
+                console.log('Error quantity =', start)
+                if (start === finish) {
+                    reject(error);
+                }
+                else {
+                    console.log('start new letsGo....')
+                    letsGo();
+                }
+            }
+        }
+        letsGo();
+    });
+}
+
+fetchUrl('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => console.log(response))
+    .catch(error => console.log('fifth error:', error))
+fetchUrl('https://jsonplaceholder1.typicode.com/todos/2')
+    .then(response => console.log(response))
+    .catch(error => console.log('fifth error:', error))
